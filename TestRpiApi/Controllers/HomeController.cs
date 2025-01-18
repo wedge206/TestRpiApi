1af5facc169a653a93c40f4ad0ff5cb52c271bc9
Matt Barnes <5efe4ever@gmail.com>
2025-01-17 22:05:38 -0800

    Just a test

--- TestRpiApi/Controllers/HomeController.cs
+++ TestRpiApi/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AprsSharp;
+using AprsSharp.KissTnc;
+using AprsSharp.Shared;
 
 namespace TestRpiApi.Controllers
 {
@@ -8,6 +11,13 @@ namespace TestRpiApi.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            var direwolf = new TcpConnection();
+            direwolf.Connect("http://localhost", 8001);
+
+            var KISS = new TcpTnc(direwolf, 0x0);
+            var text = "WSEV21>ID:Testthis";
+            KISS.SendData(System.Text.Encoding.UTF8.GetBytes(text));
+
             return new ContentResult
             {
                 ContentType = "text/html",
